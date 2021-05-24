5ab0db78901e8d3aef32ee000f000dbad02187e3
nuts-rice <msis44443@gmail.com>
2021-05-24 10:30:17 -0400

    inital commit

---
+++ src/rust_chain/mod.rs
@@ -0,0 +1,9 @@
+extern crate blake2;
+
+use std::vec::Vec;
+use std::collections::HashMap;
+use std::convert::Into;
+use std::time::SystemTime;
+use blake2::{Blake2b, Digest};
+use std::string::String;
+use std::convert::From;
